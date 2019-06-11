23ad8ce460614cdad7294b6fbff718424c0fa307
Jim-John <inbox.jimjohn@gmail.com>
2019-06-11 08:58:51 +0530

    Added Changes to Task Manager

--- basics-components/src/app/app.module.ts
+++ basics-components/src/app/app.module.ts
@@ -4,14 +4,23 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component';
-import { ServersComponent } from './servers/servers.component';
+
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { AddtaskComponent } from './taskmanager/addtask/addtask.component';
+import { ViewtaskComponent } from './taskmanager/viewtask/viewtask.component';
+import { UpdatetaskComponent } from './taskmanager/updatetask/updatetask.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent,
-    ServersComponent
+    FooterComponent,
+    HeaderComponent,
+    AddtaskComponent,
+    ViewtaskComponent,
+    UpdatetaskComponent
+    
   ],
   imports: [
     BrowserModule,
